8b12886752df72069a789a92ae1b74b415a6e580
Marianna1216 <marianna.martirosyan.h@gmail.com>
2022-11-12 01:38:48 -0800

    js 1 homework

---
+++ main.js
@@ -0,0 +1,25 @@
+  
+  
+  
+  console.log 
+
+
+  let i=23 ; 
+
+  while (i<88) { console.log ( i); i++
+
+    }; 
+
+
+
+
+
+
+
+console.log(Math.pow(2,12));
+
+let a = "*"
+while (a.length<10) {console.log (a) ; a = a + "*" ;
+  
+}
+
